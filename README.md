98d140a2db94d64b3d832f5b3cac8608a23d34b2
riiswa <wradji64@gmail.com>
2021-07-24 21:02:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+![Preview](About\Preview.jpg)
+# Scala Contamination
+
+A Rimworld Mod that adds a chance for Scaria to be spread after the fatal bite of a contaminated creature.
+
+Require Harmony.
---
+++ Source/ScariaContamination/ScariaContaminationSettings.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace ScariaContamination
+{
+    public class ScariaContaminationSettings: ModSettings
+    {
+        public float giveScariaChance = 0.5f;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref giveScariaChance, "giveScariaChance", 1f);
+            base.ExposeData();
+        }
+    }
+    
+    public class ScariaContamination : Mod
+    {
+        private ScariaContaminationSettings settings;
+        
+        public ScariaContamination(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<ScariaContaminationSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            var listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Chance to give Scaria after a bite.");
+            settings.giveScariaChance = listingStandard.Slider(settings.giveScariaChance, 0f, 1f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Scaria Contamination";
+        }
+    }
+}
